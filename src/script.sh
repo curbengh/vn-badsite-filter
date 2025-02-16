dafe3d51ce1cea52adb0494bd760de25733d077e
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2025-02-16 00:44:32 +0000

    style(rpz): generic syntax

--- src/script.sh
+++ src/script.sh
@@ -134,7 +134,7 @@ sed "1s/Blocklist/BIND Blocklist/" > "../public/vn-badsite-filter-bind.conf"
 
 ## DNS Response Policy Zone (RPZ)
 CURRENT_UNIX_TIME="$(date +%s)"
-RPZ_SYNTAX="\n\$TTL 30\n@ IN SOA rpz.malware-filter.gitlab.io. hostmaster.rpz.malware-filter.gitlab.io. $CURRENT_UNIX_TIME 86400 3600 604800 30\n NS localhost.\n"
+RPZ_SYNTAX="\n\$TTL 30\n@ IN SOA localhost. root.localhost. $CURRENT_UNIX_TIME 86400 3600 604800 30\n NS localhost.\n"
 
 cat "domains.txt" | \
 sed "s/$/ CNAME ./g" | \
